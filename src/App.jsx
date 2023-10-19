1a9bd49f7b44f58df15def5ae50954155f05704e
BrunoCiprianoDev <cipriano990@gmail.com>
2023-10-19 11:21:45 -0300

    Update baseName from BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/homeContainer";
-import FormContainer from "./components/FormContainer";
+import FormContainer from "./pages/FormContainer";
 
 function App() {
   return (
-    <>
-      <BrowserRouter basename="/portifolio">
+    <div>
+      <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/form" element={<FormContainer />} />
+          <Route path="/portifolio" element={<Home />} />
+          <Route path="/portifolio/form" element={<FormContainer />} />
           {/* <Route path='/form' element={<Form/>}/>
         <Route path='/home' element={<Home/>}/>
         <Route path='/about' element={<AboutPage/>}/>
         <Route path='/projects' element={<Projects/>}/> */}
         </Routes>
       </BrowserRouter>
-    </>
+    </div>
   );
 }
 
